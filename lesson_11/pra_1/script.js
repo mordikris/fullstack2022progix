7fc25097522395e1d8138674bd17827c7671c87b
mordikris <116097067+mordikris@users.noreply.github.com>
2022-10-30 22:32:01 +0200

    Create script.js

---
+++ lesson_11/pra_1/script.js
@@ -0,0 +1,19 @@
+// pra 1 //
+
+let guru = ["google", "facebook", "instegram", "walla", "tiktok", "net", "money", "movie"];
+console.log(guru[3]);
+
+// pra 2 //
+
+console.log(guru.length);
+console.log(guru[7]);
+
+// pra 3 //
+
+guru[2] = "intell";
+console.log(guru[2]);
+
+// pra 4 //
+
+guru.push("kiryat gat");
+console.log(guru);
