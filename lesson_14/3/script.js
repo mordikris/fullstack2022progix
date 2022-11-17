ae9c18f254b16c533232aea2e3c615b6ff53349e
mordikris <116097067+mordikris@users.noreply.github.com>
2022-11-17 17:33:23 +0200

    Create script.js

---
+++ lesson_14/3/script.js
@@ -0,0 +1,21 @@
+// pra 1 //
+let chacha = "whaat,doo,you,want,mister";
+let chacha1 = chacha.length
+console.log(chacha1);
+// pra 2+3 //
+let mor = "Fruits, Meat, Vegetables";
+let mor1 = mor.slice(14.24);
+console.log(mor1);
+// pra 4 //
+let vag = "mordechay yoav krispel";
+let vag1 = vag.toUpperCase();
+console.log(vag1);
+// pra 5 //
+let shuk = "MORDECHAY YOAV KRISPEL";
+let shuk1 = shuk.toLowerCase();
+console.log(shuk1);
+// pra 6 //
+let ulala = "Fruits, Meat, Vegetables";
+let ulala1 = ulala.replace("Meat", "Tree"); // change the word "Meat" to a "tree" in the string
+console.log(ulala1);
+
