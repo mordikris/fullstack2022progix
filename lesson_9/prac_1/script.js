1b85d221cd6a0e2568bfa4cc163c41ff0f721fee
mordikris <116097067+mordikris@users.noreply.github.com>
2022-12-27 18:37:00 +0200

    Create script.js

---
+++ lesson_9/prac_1/script.js
@@ -0,0 +1,8 @@
+let first_name = "joni", last_name = "kamoni", age = 28, burn_in = "bulivia", gender = "male";
+console.log("Full Name:" + first_name + " " + last_name + " My Age Is:" + age + " And I Live In:" + burn_in)
+
+let x = 18;
+let y = 8;
+let result = x + y;
+
+console.log(result);
