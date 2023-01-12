58bff1e1cdc4255949dacf2c82277c25ebff09e7
mordikris <116097067+mordikris@users.noreply.github.com>
2023-01-12 19:56:39 +0200

    Create script_2.js

---
+++ lessons/lesson_9/prac_1/script_2.js
@@ -0,0 +1,9 @@
+let s = 5;
+let k = 8;
+let result = s * k;
+console.log(result)
+
+let movieName = " john wick 5  ", movieRating = " 90% ", moveDirector = " mordechay krispel ";
+let result_2 = "Mymovie: " + movieName + " movie Rating: " + movieRating + " move Director: " + moveDirector
+
+console.log(result_2);
