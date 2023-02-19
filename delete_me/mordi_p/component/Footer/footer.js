1442f8cf802daff55c96735b391d966af83f6c15
mordikris <116097067+mordikris@users.noreply.github.com>
2023-02-19 20:59:32 +0200

    Create footer.js

---
+++ delete_me/mordi_p/component/Footer/footer.js
@@ -0,0 +1,27 @@
+const footer_for_all = document.createElement("div");
+footer_for_all.innerHTML = `
+<footer>
+  <div class="footer-container">
+  <p class="order">* kris project *</p>
+    <p>Stay connected with us:</p>
+    <ul class="">
+      <li>
+        <a id="contact" href="#">Contact us</a>
+      </li>
+      <li>
+        <a href="http://www.facebook.com/example">
+          Follow us <br> on Facebook <i class="fab fa-facebook-square"></i>
+        </a>
+      </li>
+      <li>
+        <a href="http://www.instagram.com/example">
+          Follow us <br> on Instagram <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+    <p>Thank you for visiting our site!</p>
+  </div>
+</footer>
+
+`
+export {footer_for_all};
