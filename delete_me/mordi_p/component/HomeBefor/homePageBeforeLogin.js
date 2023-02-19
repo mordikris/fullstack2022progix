f374ea7d3163e9a1b0ba56ae91795d7b0f8c2da4
mordikris <116097067+mordikris@users.noreply.github.com>
2023-02-19 20:59:44 +0200

    Create homePageBeforeLogin.js

---
+++ delete_me/mordi_p/component/HomeBefor/homePageBeforeLogin.js
@@ -0,0 +1,24 @@
+export function homePageBeforeLogin() {
+    document.querySelector('#main').innerHTML = `
+    <br><br><br><br>
+<div class = "order">
+<h1>  Hello To You ! </h1><br><br>
+ <div>
+   Peace
+   And welcome to Mordechai's website. <br>
+   There are all kinds of options to do things here on the site. <br>
+   There are games, there is a calculator, <br> and there is even a website where you just register without a
+   specific result
+ </div><br>
+ <div>
+   - Welcome to our website! We are glad that you have chosen to visit us and explore what we have to offer.<br>
+   Our goal is to provide a sophisticated and engaging experience for all of our visitors.
+   <br><br>
+   - We hope that you will join us and become an integral part of our community. Thank you for stopping by!
+   <br><br>
+   <p id="pic"></p>
+   <br><br>
+ </div>
+</div>
+`
+}
