cc067e9dd4704a7d8b5b382b100f12eefd809e31
mordikris <116097067+mordikris@users.noreply.github.com>
2023-02-19 20:59:29 +0200

    Create contact.js

---
+++ delete_me/mordi_p/component/contact/contact.js
@@ -0,0 +1,20 @@
+const contact_page = document.createElement("div");
+contact_page.innerHTML = `
+<br><br><br><br>
+<div class="container"><br>
+            <div class="login"><br>
+                <h1 id="h_one">Contact page</h1>
+                <br><br><br>
+                <p>Email : mordikrispel@gmail.com </p>
+                <p>address : Miami Design District </p>
+                <p>phone : +972524721666 </p>
+                <p>oppice phone : 0868884432 </p>
+                <br><br>
+                <div class="container">
+                    <h3 id="h_one"> * Hope you enjoy our servise * </h3>
+                    <br><br><br>
+                </div>
+            </div><br>
+        </div><br><br><br>
+`;
+export {contact_page};
