c1994cd474d5dae0727ffa690eb98459473c9dbf
mordikris <116097067+mordikris@users.noreply.github.com>
2023-03-19 22:05:59 +0200

    Create loop.py

---
+++ python/lesson_1/loop.py
@@ -0,0 +1,11 @@
+# Q1 #
+holla = input("type here : ")
+
+# for char in holla:
+#     print(int(char[0]+char[1]+char[2]))
+
+    ## range()
+    ## len() 
+hullala = len(holla)
+for i in range(hullala):
+     print(holla[i], i)
