dee26b1195ddfb1fe5e8905d07685e49664911bf
mordikris <116097067+mordikris@users.noreply.github.com>
2023-03-19 22:06:06 +0200

    Create usernumber.py

---
+++ python/lesson_1/usernumber.py
@@ -0,0 +1,7 @@
+# Q1 #
+# get number from user and print in his own possession #
+
+user_number = input("please enter your spacial number here :")
+print("your number is : ", user_number)
+# nou we print in his own possession #
+print(int(user_number) ** 2)
