ba4c81021bcba8d77291fedb3ce5e9e2c13023fa
mordikris <116097067+mordikris@users.noreply.github.com>
2023-03-19 22:06:02 +0200

    Create treenum.py

---
+++ python/lesson_1/treenum.py
@@ -0,0 +1,13 @@
+#  Q1  #
+# take the tree number from user and get the resoult #
+number_1 = input("enter number : ")
+x = number_1[0]
+y = number_1[1] 
+z = number_1[2]
+print(int(x)+int(y)+int(z))
+
+# number_2 = input("enter number : ")
+# number_3 = input("enter number : ")
+# print("your number is :" , number_1,number_2,number_3)
+# # the resoult #
+# print("the resoult is :",int(number_1)+int(number_2)+int(number_3))   
