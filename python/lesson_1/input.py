b28e1cd0a6c19b4019f4d80add9f669f06215b26
mordikris <116097067+mordikris@users.noreply.github.com>
2023-03-19 22:05:57 +0200

    Create input.py

---
+++ python/lesson_1/input.py
@@ -0,0 +1,14 @@
+# [Q1] set first_name, last_name, age, height from user input
+# [Q2] print users age in 10 years from now
+
+first_name = input("please enter your firs name :")
+last_name = input("please enter your last name :")
+age = input("please enter your age :")
+height = input("please enter your height:")
+
+print(first_name)
+print(last_name)
+print(int(age)+2)
+print(int(height)*2)
+
+# Completed #
